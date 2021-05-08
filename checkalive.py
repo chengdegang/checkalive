7c4904d19df24931ad0cf5cf9eea32f656f72d3f
ezxr.chengdegang <chengdegang@ezxr>
2021-05-08 17:20:25 +0800

    suc_v3_发件人更换为企业

--- checkalive.py
+++ checkalive.py
@@ -34,22 +34,20 @@ for faild in urls_failed:
 
 #设置smtplib所需的参数
 #下面的发件人，收件人是用于邮件传输的。
-smtpserver = 'smtp.qq.com'
-username = 'chengdgccc@qq.com'
-password='glgfdpxlnhiogaji'
-sender='1093637306@qq.com'
-receivers = ['chengdegang@ezxr.com']
-#收件人为多个收件人
-# receiver=['XXX@126.com']
+smtpserver = 'smtphz.qiye.163.com'
+username = 'chengdegang@ezxr.com'
+password= 'Bbu3Cu8CzYtydxLp'
+sender= 'chengdegang@ezxr.com'
+receivers = ['chengdegang@ezxr.com','wb.wangbinbin@ezxr.com']
 
 subject = 'Service check reminder'
 
 msg = MIMEMultipart('mixed')
 msg['Subject'] = subject
-msg['From'] = '1093637306@qq.com <1093637306@qq.com>'
-msg['To'] = 'qaccc'
+msg['From'] = 'chengdegang@ezxr.com <chengdegang@ezxr.com>'
+msg['To'] = 'qa'
 #收件人为多个收件人,通过join将列表转换为以;为间隔的字符串
-# msg['To'] = ";".join(receiver)
+msg['To'] = ";".join(receivers)
 
 text = f"Hi!\n{msg_text}\nHere is the link you wanted:\n{urls_failed}"
 text_plain = MIMEText(text,'plain', 'utf-8')
--- main.py
+++ main.py
@@ -6,47 +6,53 @@ from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from email.header import Header
 
-# #设置smtplib所需的参数
-# #下面的发件人，收件人是用于邮件传输的。
+#设置smtplib所需的参数
+#下面的发件人，收件人是用于邮件传输的。
 # smtpserver = 'smtp.qq.com'
 # username = 'chengdgccc@qq.com'
 # password='glgfdpxlnhiogaji'
 # sender='chengdgccc@qq.com'
 # receivers = ['chengdegang@ezxr.com']
-#
-# subject = 'Service check reminder'
-#
-# msg = MIMEMultipart('mixed')
-# msg['Subject'] = subject
-# msg['From'] = '1093637306@qq.com <1093637306@qq.com>'
-# msg['To'] = 'qaccc'
-# #收件人为多个收件人,通过join将列表转换为以;为间隔的字符串
-# # msg['To'] = ";".join(receiver)
-#
-# text = "Hi!\nHow are you?\nHere is the link you wanted:\nhttp://www.baidu.com"
-# text_plain = MIMEText(text,'plain', 'utf-8')
-# msg.attach(text_plain)
-#
-# smtp = smtplib.SMTP_SSL(smtpserver)
-# smtp.connect(smtpserver,'465')
-# #我们用set_debuglevel(1)就可以打印出和SMTP服务器交互的所有信息。
-# #smtp.set_debuglevel(1)
-# smtp.login(username, password)
-# smtp.sendmail(sender,receivers, msg.as_string())
-# print("send success")
-# smtp.quit()
 
-urls = ['http://10.244.12.50:9223/#/',
-        'http://10.244.12.21:8081/',
-        'http://10.244.12.21:8080/',
-        'http://10.244.12.21:8088/',
-        'http://10.244.12.21:8089/']
+smtpserver = 'smtphz.qiye.163.com'
+username = 'chengdegang@ezxr.com'
+password= 'Bbu3Cu8CzYtydxLp'
+sender= 'chengdegang@ezxr.com'
+receivers = ['chengdegang@ezxr.com']
 
-def result(urls):
-    for url in urls:
-        # print(url)
-        return url
+subject = 'Service check reminder'
 
-print(result())
+msg = MIMEMultipart('mixed')
+msg['Subject'] = subject
+msg['From'] = 'chengdegang@ezxr.com <chengdegang@ezxr.com>'
+msg['To'] = 'qa'
+#收件人为多个收件人,通过join将列表转换为以;为间隔的字符串
+# msg['To'] = ";".join(receiver)
+
+text = "Hi!\nHow are you?\nHere is the link you wanted:\nhttp://www.baidu.com"
+text_plain = MIMEText(text,'plain', 'utf-8')
+msg.attach(text_plain)
+
+smtp = smtplib.SMTP_SSL(smtpserver)
+smtp.connect(smtpserver,'465')
+#我们用set_debuglevel(1)就可以打印出和SMTP服务器交互的所有信息。
+#smtp.set_debuglevel(1)
+smtp.login(username, password)
+smtp.sendmail(sender,receivers, msg.as_string())
+print("send success")
+smtp.quit()
+
+# urls = ['http://10.244.12.50:9223/#/',
+#         'http://10.244.12.21:8081/',
+#         'http://10.244.12.21:8080/',
+#         'http://10.244.12.21:8088/',
+#         'http://10.244.12.21:8089/']
+#
+# def result(urls):
+#     for url in urls:
+#         # print(url)
+#         return url
+#
+# print(result())
 
 
