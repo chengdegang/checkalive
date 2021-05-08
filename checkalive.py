565a1128b795f3cd0968860ad6ff834d91757cf7
ezxr.chengdegang <chengdegang@ezxr>
2021-05-08 10:03:03 +0800

    suc_v1

--- checkalive.py
+++ checkalive.py
@@ -1,11 +1,7 @@
 import requests
-# import aiosmtplib
-# from email.message import EmailMessage
-# import asyncio
-# from aiosmtplib import SMTP
 import smtplib
+from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from email.header import Header
 
 # url = 'http://10.244.12.50:9223/#/'
 urls = ['http://10.244.12.50:9223/#/',
@@ -23,51 +19,48 @@ for url in urls:
     # except ConnectionRefusedError:
         print(f"失败(无法访问) {url}")
         urls_failed.append(url)
-        url_failed_num = + 1
+        url_failed_num = url_failed_num + 1
         continue
     #200成功以及403无权限访问均表示该网站没有挂
     if r.status_code == 200 or r.status_code == 403:
         print(f"成功 该url为：{url}")
     else:
         print(f"失败 该url为：{url}")
-        url_failed_num = + 1
+        url_failed_num += 1
         urls_failed.append(url)
 
+msg_text = f"今天一共失败了{url_failed_num}个"
 print(f"一共失败了{url_failed_num}个")
 print(f"失败的url如下:{urls_failed}")
 
-# message = EmailMessage()
-# message["From"] = "root@localhost"
-# message["To"] = "chengdegang@ezxr.com"
-# message["Subject"] = "Hello World!"
-# message.set_content("Sent via aiosmtplib")
-# loop = asyncio.get_event_loop()
-# loop.run_until_complete(aiosmtplib.send(message, hostname="127.0.0.1", port=25))
-# def sent_email(mail_body):
-#     sender = '***********'
-#     receiver = '**********'
-#     smtpServer = 'smtp.163.com'
-#     username = '*******'
-#     password = '*******'
-#     mail_title = '今日通知'
-#     mail_body = mail_body
-#
-#     message = MIMEText(mail_body, 'plain', 'utf-8')
-#     message["Accept-Language"] = "zh-CN"
-#     message["Accept-Charset"] = "ISO-8859-1,utf-8"
-#     message['From'] = sender
-#     message['To'] = receiver
-#     message['Subject'] = Header(mail_title, 'utf-8')
-#
-#     try:
-#         smtp = smtplib.SMTP()
-#         smtp.connect(smtpServer)
-#         smtp.login(username, password)
-#         smtp.sendmail(sender, receiver, message.as_string())
-#         print('邮件发送成功')
-#         smtp.quit()
-#     except smtplib.SMTPException:
-#         print("邮件发送失败！！！")
+#设置smtplib所需的参数
+#下面的发件人，收件人是用于邮件传输的。
+smtpserver = 'smtp.qq.com'
+username = 'chengdgccc@qq.com'
+password='glgfdpxlnhiogaji'
+sender='1093637306@qq.com'
+receivers = ['chengdegang@ezxr.com']
+#收件人为多个收件人
+# receiver=['XXX@126.com']
 
+subject = 'Service check reminder'
 
-# print(f"Status code: {r.status_code}")
+msg = MIMEMultipart('mixed')
+msg['Subject'] = subject
+msg['From'] = '1093637306@qq.com <1093637306@qq.com>'
+msg['To'] = '18868890069@163.com'
+#收件人为多个收件人,通过join将列表转换为以;为间隔的字符串
+# msg['To'] = ";".join(receiver)
+
+text = f"Hi!\n{msg_text}\nHere is the link you wanted:\n{urls_failed}"
+text_plain = MIMEText(text,'plain', 'utf-8')
+msg.attach(text_plain)
+
+smtp = smtplib.SMTP_SSL(smtpserver)
+smtp.connect(smtpserver,'465')
+#我们用set_debuglevel(1)就可以打印出和SMTP服务器交互的所有信息。
+#smtp.set_debuglevel(1)
+smtp.login(username, password)
+smtp.sendmail(sender,receivers, msg.as_string())
+print("send success")
+smtp.quit()
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# This is a sample Python script.
 from smtplib import SMTP_SSL
 from email.mime.text import MIMEText
 import smtplib
@@ -9,14 +8,13 @@ from email.header import Header
 
 #设置smtplib所需的参数
 #下面的发件人，收件人是用于邮件传输的。
-smtpserver = 'smtp.exmail.qq.com'
-username = '1093637306@qq.com'
+smtpserver = 'smtp.qq.com'
+username = 'chengdgccc@qq.com'
 password='glgfdpxlnhiogaji'
-sender='18868890069@163.com'
-#收件人为多个收件人
-# receiver=['XXX@126.com']
+sender='chengdgccc@qq.com'
+receivers = ['chengdegang@ezxr.com']
 
-subject = 'Python email test'
+subject = 'Service check reminder'
 
 msg = MIMEMultipart('mixed')
 msg['Subject'] = subject
@@ -29,52 +27,11 @@ text = "Hi!\nHow are you?\nHere is the link you wanted:\nhttp://www.baidu.com"
 text_plain = MIMEText(text,'plain', 'utf-8')
 msg.attach(text_plain)
 
-smtp = smtplib.SMTP()
+smtp = smtplib.SMTP_SSL(smtpserver)
 smtp.connect(smtpserver,'465')
 #我们用set_debuglevel(1)就可以打印出和SMTP服务器交互的所有信息。
 #smtp.set_debuglevel(1)
 smtp.login(username, password)
-smtp.sendmail(sender, msg.as_string())
+smtp.sendmail(sender,receivers, msg.as_string())
+print("send success")
 smtp.quit()
-
-# def sendMail(message,Subject,sender_show,recipient_show,to_addrs,cc_show=''):
-#     '''
-#     :param message: str 邮件内容
-#     :param Subject: str 邮件主题描述
-#     :param sender_show: str 发件人显示，不起实际作用如："xxx"
-#     :param recipient_show: str 收件人显示，不起实际作用 多个收件人用','隔开如："xxx,xxxx"
-#     :param to_addrs: str 实际收件人
-#     :param cc_show: str 抄送人显示，不起实际作用，多个抄送人用','隔开如："xxx,xxxx"
-#     '''
-#     # 填写真实的发邮件服务器用户名、密码
-#     user = '1093637306@qq.com'
-#     password = 'Chengdegang01'
-#     # 邮件内容
-#     msg = MIMEText(message, 'plain', _charset="utf-8")
-#     # 邮件主题描述
-#     msg["Subject"] = Subject
-#     # 发件人显示，不起实际作用
-#     msg["from"] = sender_show
-#     # 收件人显示，不起实际作用
-#     msg["to"] = recipient_show
-#     # 抄送人显示，不起实际作用
-#     msg["Cc"] = cc_show
-#     with SMTP_SSL(host="smtp.exmail.qq.com",port=25) as smtp:
-#         # 登录发邮件服务器
-#         smtp.login(user = user, password = password)
-#         # 实际发送、接收邮件配置
-#         smtp.sendmail(from_addr = user, to_addrs=to_addrs.split(','), msg=msg.as_string())
-#         print("success")
-#
-# if __name__ =='__main':
-#     message = 'Python 测试邮件...'
-#     Subject = 'ces'
-#     # 显示发送人
-#     sender_show = 'ctest'
-#     # 显示收件人
-#     recipient_show = 'c'
-#     # 实际发给的收件人
-#     to_addrs = '18868890069@163.com'
-#     sendMail(message,Subject,sender_show,recipient_show,to_addrs)
-
-# smtpObj = smtplib.SMTP( [host='smtp.exmail.qq.com' [, port=465 [, local_hostname]]] )
